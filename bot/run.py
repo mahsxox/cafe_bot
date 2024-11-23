cd87a8ae75f80e7b48c0bc560457e21d022fd5a1
mahsxox <paderinmaksimoghph@gmail.com>
2024-11-23 22:23:26 +0300

    Update run.py

--- bot/run.py
+++ bot/run.py
@@ -3,16 +3,18 @@ import logging
 from aiogram import Bot, Dispatcher
 from app.handlers import router
 
-bot = Bot(token= '8046550392:AAFGrOjyqLmSqVs2y8JXXOzG7czsBWw5r8o')
+# Создаём бота и диспетчер
+bot = Bot(token='8046550392:AAFGrOjyqLmSqVs2y8JXXOzG7czsBWw5r8o')
 dp = Dispatcher()
 
 async def main():
-    dp.include_router(router)
-    await dp.start_polling(bot)
+    dp.include_router(router)  # Подключаем обработчики
+    await dp.start_polling(bot)  # Запускаем бота
 
+# Точка входа
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO)  # Логирование
     try:
-        asyncio.run(main())
+        asyncio.run(main())  # Запуск бота
     except KeyboardInterrupt:
-        print('Exit')
+        print('Exit')  # Завершение
